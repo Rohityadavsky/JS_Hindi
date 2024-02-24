3a226eca9395765fe86e4696895ef5d887138270
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2024-02-24 16:08:12 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,15 @@
+// const a = console.log(3+3)  
+// console.log(a)
+
+// console.log(6+7);console.log("rohit")
+
+// datatype in javascript 
+
+// number  = 12235
+// bigint = 
+// string = "rohit"
+// boolean = true,false
+// null = standalone value empty
+// undefined only declear 
+// symbole = unique 
+// object  
