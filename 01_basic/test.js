f8dd4e96831fb094414751e4134bba667ec668b0
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2024-02-24 15:36:36 +0000

    something change

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,11 @@
-console.log("Rohit yadav Hello world !!!")
+// const acountId = 12345
+// // acountId = 56789
+// let userName = "rohit yadav"
+// var accountEmail = "rohit@google.com"
+// accountCity= "jaunpur"
+// accountPassword = 123456789
+// console.table([acountId,userName,accountEmail,accountCity,accountPassword])
+
+var a = "rohit"
+console.log(a)
+console.table([a])
---
+++ 01_basic/variable.js
@@ -0,0 +1,4 @@
+const accountId = 12345
+let accountName = "rohit"
+accountEmail = "rohit@google.com"
+console.table([accountId,accountName,accountEmail])
