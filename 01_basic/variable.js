d5ae1e0bb7d32f683c5f4b5376119e76a5d144c8
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2024-02-24 15:42:30 +0000

    add relevent things

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -2,3 +2,7 @@ const accountId = 12345
 let accountName = "rohit"
 accountEmail = "rohit@google.com"
 console.table([accountId,accountName,accountEmail])
+
+/*
+prefer not to use var because of issue in block scope and functional scope.
+*/
