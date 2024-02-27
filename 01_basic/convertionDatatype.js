964d16ed0c4804c73fe1908c6bdce03cda060a06
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2024-02-27 05:41:06 +0000

    convertion data type

---
+++ 01_basic/convertionDatatype.js
@@ -0,0 +1,13 @@
+let a = 33
+
+//console.log(typeof a)
+//console.log(typeof(a))
+
+let valueInNumber = Number(a)
+//console.log(typeof valueInNumber)
+
+let loggedIn = 122345
+let check = Boolean(loggedIn)
+//console.log(check)
+
+
