fb93aa782140ec4ac325d1aed67ef5ee161a12ed
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2024-02-27 17:04:36 +0000

    data type summary

---
+++ 01_basic/datatype_Summary.js
@@ -0,0 +1,35 @@
+// primitive data type 
+
+// number , string , boolean , BigInt, symbol, null, undefined 
+
+const id = 5;
+const idUser = 5;
+
+const heroes = "rohit"
+
+const kya = true;
+
+// let p1 = symbol("43")
+// console.log(p1)
+
+
+const p2="";
+
+
+// Reference (non primitive data type )
+
+// Array, Object, function 
+
+const arr = ["rohit","yadav"]
+
+let obj = {
+    name : "rohit",
+    password:453434,
+    email : "rohit@gmail.com"
+}
+
+let myFunction = function(){
+    console.log("hello Wrold !!!");
+}
+
+console.log(typeof myFunction)
