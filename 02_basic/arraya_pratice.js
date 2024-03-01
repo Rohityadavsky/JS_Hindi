2e5348a8de709562833dfed7c677e072f410e099
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2024-03-01 16:54:32 +0000

    add some array point

---
+++ 02_basic/arraya_pratice.js
@@ -0,0 +1,12 @@
+// const arr = [2,3,4,4]
+// console.log(arr)
+
+const marvel_heros =["thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+const all_heros = marvel_heros.concat(dc_heros)
+console.log(all_heros)
+
